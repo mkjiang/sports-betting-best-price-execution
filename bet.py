eee02c774c5a63295ea00a941f407950a2710a33
mkjiang <maxjiang@hotmail.com>
2017-04-09 23:59:46 +1000

    Update bet.py

--- bet.py
+++ bet.py
@@ -15,3 +15,25 @@ class EventOdds():
         self.win = win
         self.draw = draw
         self.lose = lose
+
+class Portfolio():
+
+    def __init__(self):
+        self.fund_count = 0
+
+        self.profit = 0
+
+        self.win_item = LotteryItem()
+        self.draw_item = LotteryItem()
+        self.lose_item = LotteryItem()
+
+        self.win_percentage = 0
+        self.draw_percentage = 0
+        self.lose_percentage = 0
+
+    def display(self):
+        print "profit:\t%s\nwin:\t%s %s\t%s\t%s\ndraw:\t%s %s\t%s\t%s\nlose:\t%s %s\t%s\t%s" % \
+              (self.profit,
+               self.win_item.id, self.win_item.company, self.win_item.cw_odds, self.win_percentage,
+               self.draw_item.id, self.draw_item.company, self.draw_item.cd_odds, self.draw_percentage,
+               self.lose_item.id, self.lose_item.company, self.lose_item.cl_odds, self.lose_percentage)
